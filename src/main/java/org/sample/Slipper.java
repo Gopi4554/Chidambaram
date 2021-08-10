e95be3d3ef962344e0b2ff7047ea57763f991a21
Gopi4554 <gopiandrew297@gmail.com>
2021-08-10 20:04:36 +0530

    Kerala Modified

--- src/main/java/org/sample/Slipper.java
+++ src/main/java/org/sample/Slipper.java
@@ -1,5 +1,19 @@
 package org.sample;
 
 public class Slipper {
-
+	
+	public void southIndian() {
+		System.out.println("South Indian");
+	}
+	public void northIndian() {
+		System.out.println("North Indian");
+	}
+public static void main(String[] args) {
+	
+		Slipper s=new Slipper();
+		s.southIndian();
+		s.northIndian();
+		
+		
+}
 }
